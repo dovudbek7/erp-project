cfba4795a3f00768642aa88e5abadcf27aac5f3b
Dovudbek <dovudbek@Dovudbeks-MacBook-Air.local>
2026-05-07 11:38:46 +0500

    add

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,11 +1,17 @@
-
+import { RiDashboardFill } from "react-icons/ri"
+import { Link, Outlet } from "react-router"
 
 const HomePage = () => {
-    return (
-        <div>
-            Home Page
+  return (
+    <>
+      <div className="grid grid-cols-2">
+        
+        <div className="col">
+          <Outlet />
         </div>
-    );
+      </div>
+    </>
+  )
 }
 
-export default HomePage;
+export default HomePage
